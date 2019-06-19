55d06cb93f941b693c57c3284bb88f0f3b622bc5
Simon M <Simon M>
2019-06-19 16:22:31 +0200

    test 5.2 angef

--- source/tests.cpp
+++ source/tests.cpp
@@ -3,8 +3,13 @@
 
 // * --------------- 5.1 und 5.2 --------------- * //
 TEST_CASE("TestCase_1    5.1 und 5.2", "[aufgabe 5.1,5.2]") {
- 
-  REQUIRE(true);
+
+  Sphere sph1; 
+  REQUIRE (sph1.center_ == 0.0f);
+  REQUIRE (sph1.center_ == 0.0f);
+  REQUIRE (sph1.center_ == 0.0f);
+  REQUIRE (sph1.radius_ == 1.0f);
+
 }
 
 int main(int argc, char *argv[])
