edba12a59a16ea72d58629fe9fede5bfd9c1c887
Simon M <Simon M>
2019-07-10 00:40:20 +0200

    scene sdf einlesen und in containern speichern soweit ok

--- framework/scene.hpp
+++ framework/scene.hpp
@@ -35,20 +35,59 @@ static void get_SDF_File(std::string const& path,Scene& scene){      // Freie fk
     std::string identifier;
     current_line_string_stream >> identifier;
 
+    //file.open(path);
+
     if( "define" == identifier ) {
       current_line_string_stream >> identifier;
       
       if("material" == identifier) {
         std::cout << "this is in the while-loop of get_SDF_File-method  \n";
-        
-        
+
+        std::string col_as_name;
+
+        current_line_string_stream >> col_as_name; 
+
         float ka_red, ka_green, ka_blue;
-        current_line_string_stream >> ka_red >> ka_green >> ka_blue;
-        //current_line_string_stream >> ka_green;
-        //current_line_string_stream >> ka_blue;
+        float kd_red, kd_green, kd_blue;
+        float ks_red, ks_green, ks_blue;
+        float f1;
+
+        current_line_string_stream >> ka_red;
+        current_line_string_stream >> ka_green;
+        current_line_string_stream >> ka_blue;
+
+        current_line_string_stream >> kd_red;
+        current_line_string_stream >> kd_green;
+        current_line_string_stream >> kd_blue;
+
+        current_line_string_stream >> ks_red;
+        current_line_string_stream >> ks_green;
+        current_line_string_stream >> ks_blue;
+
+        current_line_string_stream >> f1;
+       
+        std::cout << col_as_name << std::endl;
+
         std::cout << ka_red << std::endl;
-        std::cout << ka_green << std::endl;;
-        std::cout << ka_blue << std::endl;;
+        std::cout << ka_green << std::endl;
+        std::cout << ka_blue << std::endl;
+
+        std::cout << kd_red << std::endl;
+        std::cout << kd_green << std::endl;
+        std::cout << kd_blue << std::endl;
+
+        std::cout << ks_red << std::endl;
+        std::cout << ks_green << std::endl;
+        std::cout << ks_blue << std::endl;
+
+        std::cout << f1 << std::endl;
+
+        Material current_loop_mat{col_as_name,{ka_red,ka_green,ka_blue},{kd_red,kd_green,kd_blue},{ks_red,ks_green,ks_blue},f1};
+        auto m1 = std::make_shared<Material>(current_loop_mat);
+        std::cout << current_loop_mat << std::endl;
+        scene.vec_mat.push_back(m1);
+        scene.set_mat.insert(m1);
+        scene.map_mat.emplace(col_as_name,m1);
         
       }
     }
